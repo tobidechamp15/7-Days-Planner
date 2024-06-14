cc5a4c2d3da6dcc17a1551fe84b7c81bc3803a1e
Oluwadare Oluwatobiloba <tobidechamp15@gmail.com>
2024-06-14 21:50:43 +0100

    Navbar Feature

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import logo from "../assets/logo.svg";
+import userAvatar from "../assets/userAvatar.svg";
+import notificationIcon from "../assets/notification.svg";
+import searchIcon from "../assets/searchIcon.svg";
+
+const Navbar = () => {
+  return (
+    <nav className="flex justify-between p-[60px] bg-[#004E96]">
+      <img src={logo} className="" />
+      <div className="flex gap-4 items-center justify-center">
+        <img src={searchIcon} className="hidden md:flex" />
+        <img src={notificationIcon} className="hidden md:flex" />
+        <div className="flex gap-2 items-center justify-center ">
+          <span className="text-white border rounded-[18px] p-2 hidden md:block">
+            Oluwaseun
+          </span>
+          <img src={userAvatar} alt="" />
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Dashboard from "./Components/Dashboard";
 
 // Create a router
 const router = createBrowserRouter([
   {
     path: "/",
+    element: <Dashboard />,
   },
 ]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
