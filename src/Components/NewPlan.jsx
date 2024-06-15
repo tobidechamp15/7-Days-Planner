f3541dcf376f552a3172daf4f3984df5421e290d
Oluwadare Oluwatobiloba <tobidechamp15@gmail.com>
2024-06-15 21:14:20 +0100

    Dashboard edit

---
+++ src/Components/NewPlan.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NewPlan = () => {
+  return (
+    <div>
+      <span>New Plan</span>
+    </div>
+  );
+};
+
+export default NewPlan;
--- src/index.js
+++ src/index.js
@@ -4,12 +4,38 @@ import "./index.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Dashboard from "./Components/Dashboard";
+import Home from "./Components/Home";
+import NewPlan from "./Components/NewPlan";
+import Plans from "./Components/Plans";
+import Profile from "./Components/Profile";
 
 // Create a router
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Dashboard />,
+  },
+  {
+    path: "/dashboard",
+    element: <Dashboard />,
+    children: [
+      {
+        path: "/dashboard/home",
+        element: <Home />,
+      },
+      {
+        path: "/dashboard/new-plan",
+        element: <NewPlan />,
+      },
+      {
+        path: "/dashboard/plans",
+        element: <Plans />,
+      },
+      {
+        path: "/dashboard/profile",
+        element: <Profile />,
+      },
+    ],
   },
 ]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
